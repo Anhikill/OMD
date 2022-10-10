b91174381d26a3f8deea2870d694e8d1a798c4ff
Ki <Ki@ECHO-DEMONIAQUE>
2022-10-10 10:50:47 +0200

    changement checkstyle

--- TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
+++ TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
@@ -4,10 +4,8 @@ package OMD.ANNICGRANDJEAN.TP3v2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
