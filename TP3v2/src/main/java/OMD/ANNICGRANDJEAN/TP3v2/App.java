ce0ea9566dea883aba648921381c12b2068a904f
Ki <anhikill01@gmail.com>
2022-10-11 14:51:39 +0200

    ajout de jar

--- TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
+++ TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
@@ -1,12 +1,20 @@
 package OMD.ANNICGRANDJEAN.TP3v2;
 
+import java.util.logging.Logger;
+
+
 /**
  * Hello world!
  *
  */
 public class App {
+
+
+  private static final Logger logger = Logger.getLogger(App.class.getPackage().getName());
+
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    logger.info("My Message");
   }
   // update
   // annexe
