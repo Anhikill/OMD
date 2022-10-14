9267b2fc4ec09e991a30cf26874906e9e87f2bac
Ki <anhikill01@gmail.com>
2022-10-14 11:22:24 +0200

    grande renov

--- TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
+++ TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class App {
 
 
-  private static final Logger logger = Logger.getLogger(App.class.getPackage().getName());
+  private static final  Logger logger = Logger.getLogger(App.class.getPackage().getName());
 
 
   public static void main(String[] args) {
